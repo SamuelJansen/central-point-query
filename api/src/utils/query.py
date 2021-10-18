1cbf1d5fdab3efe653bfd4e24952d9a1883983ca
samuel.jansen <samuel.jansenn@gmail.com>
2021-10-17 22:23:21 -0300

    feature: implemented

--- api/src/utils/query.py
+++ api/src/utils/query.py
@@ -1,41 +1,151 @@
 import numpy as np
 
+from python_helper import Constant as c
+from python_helper import log, RandomHelper, StringHelper, ObjectHelper
 
 ALL_HITS: int = 0
 INNER_ONE: int = -1
 
 
-def hashedDataset(
-    query: np.array,
-    hashedDataset: dict,
-    amount: int = ALL_HITS,
-    axis: int = INNER_ONE
-) -> np.array:
-    return dataset(
-        query,
-        hashedDataset.get(
-            getHash(query),
-            np.array([])
-        ),
-        amount=amount,
-        axis=axis
-    )
+class DatasetKeys:
+    DATASET_MEAN = 'datasetMean'
+    VALUES = 'values'
+    DATA = 'data'
+    TARGET = 'target'
 
 
-def dataset(
-    query: np.array,
-    dataset: np.array,
-    amount: int = ALL_HITS,
-    axis: int = INNER_ONE
-) -> np.array:
+class Dataset:
+
+    def __init__(self, data: np.array, target: np.array, displayShape: tuple = None):
+        self.data: np.array = data
+        self.target: np.array = target
+        self.displayShape: tuple = displayShape
+        log.debug(self.__init__, f'data.shape: {self.data.shape}')
+        log.debug(self.__init__, f'target.shape: {self.target.shape}')
+        log.debug(self.__init__,  f'data.min: {self.data.min()}, data.max: {self.data.max()}')
+        log.debug(self.__init__,  f'target.min: {self.target.min()}, target.max: {self.target.max()}')
+
+    def displaySample(self):
+        sampledIndex = RandomHelper.integer(minimum=0, maximum=self.data.shape[0]-1)
+        log.debug(self.displaySample, f'data sampled: {arrayToString(self.data[sampledIndex], shape=self.displayShape)}')
+        log.debug(self.displaySample, f'respective target: {self.target[sampledIndex]}')
+
+
+class HashedDataset:
+
+    def __init__(self, dataset: Dataset, displayShape: tuple):
+        self.dataset = {
+            DatasetKeys.DATASET_MEAN: buildDatasetMean(dataset.data),
+            DatasetKeys.VALUES: {}
+        }
+        self.displayShape = displayShape
+        self.add(dataset)
+
+    def get(self):
+        return self.dataset
+
+    def getDatasetMean(self):
+        return self.dataset.get(DatasetKeys.DATASET_MEAN)
+
+    def add(self, dataset: Dataset):
+        for index, sample in enumerate(dataset.data):
+            self.append(index, sample, dataset)
+
+    def append(self, index: int, sample: np.array, dataset: Dataset):
+        hash = getDatasetMeanHash(sample, self.getDatasetMean())
+        if not hash in self.dataset:
+            self.dataset[hash] = {
+                DatasetKeys.VALUES: np.array([sample])
+            }
+        else :
+            self.dataset[hash][DatasetKeys.VALUES] = appendArray(self.dataset[hash][DatasetKeys.VALUES], sample)
+        self.dataset[DatasetKeys.VALUES][arrayToString(sample, shape=self.displayShape)] = {
+            DatasetKeys.DATA: sample,
+            DatasetKeys.TARGET: dataset.target[index]
+        }
+
+    def query(self, query: np.array, amount: int = ALL_HITS, axis: int = INNER_ONE) -> np.array:
+        return queryFromDataset(
+            query,
+            self.dataset.get(
+                getDatasetMeanHash(query, self.getDatasetMean())
+            ).get(DatasetKeys.VALUES),
+            amount=amount,
+            axis=axis
+        )
+
+
+def queryFromDataset(query: np.array, dataset: np.array, amount: int = ALL_HITS, axis: int = INNER_ONE) -> np.array:
     assert 2 == len(dataset.shape), f'Data shape {dataset.shape} should have only 2 dimentions'
     return dataset[np.argsort(np.sum((query - dataset)**2, axis=axis))[:_getAmount(dataset, amount)]]
 
 
-def getHash(query: np.array) -> str:
-    return str(np.array)
+def arrayToString(data: np.array, roundTo: int = 2, shape: tuple = None, title: str = c.BLANK):
+    log.log(arrayToString, f'initial data.shape: {data.shape}')
+    if ObjectHelper.isNotNone(shape) and not shape == data.shape:
+        try:
+            data = data.reshape(shape)
+        except Exception as exception:
+            log.error(arrayToString, f'Not possible to reshape {c.NEW_LINE}{data} of shape {data.shape} into shape {shape}', exception, muteStackTrace=True)
+            raise exception
+    log.log(arrayToString, f'data.shape: {data.shape}')
+    if 2 <= len(data.shape):
+        return StringHelper.join(
+            [
+                title,
+                *[
+                    StringHelper.join(
+                        [
+                            f'{str(int(round((10**roundTo) * round(value, roundTo)))).center(roundTo+1)}' for value in line
+                        ],
+                        character=c.BLANK
+                    ) for line in data
+                ]
+            ],
+            character=c.NEW_LINE
+        )
+    else:
+        return StringHelper.join(
+            [
+                title,
+                str(data)
+            ],
+            character=c.NEW_LINE
+        )
+
+
+def buildDatasetMean(data: np.array, displayShape: tuple = None) -> np.array:
+    log.info(buildDatasetMean, 'Calculating data mean')
+    # centralPoint = np.mean(flattenMnist.data, axis=0)
+    if 1 == len(data.shape):
+        return data
+    dataMean = None
+    try:
+        dataMean = np.mean(data, axis=0)
+    except Exception as exception:
+        log.failure(buildDatasetMean, 'No possible to evaluate np.mean(data, axis=0). Going for a splicit approach')
+        for index, dataElement in enumerate(list(data)):
+            if ObjectHelper.isNone(dataMean):
+                dataMean = dataElement
+            else:
+                dataMean += (dataElement - dataMean) / index
+    log.debug(buildDatasetMean, f'Data mean: {arrayToString(dataMean, shape=displayShape)}')
+    return dataMean
+
+
+def getDatasetMeanHash(query: np.array, centralPoint: np.array) -> str:
+    if ObjectHelper.isNone(centralPoint):
+        raise Exception('Central point cannot be null')
+    try:
+        return str([1 if v else 0 for v in query > centralPoint])
+    except Exception as exception:
+        log.error(getDatasetMeanHash, f'not possible to compare: {query} with {centralPoint}', exception, muteStackTrace=True)
+        raise exception
+
+
+def appendArray(array: np.array, arrayToAppend: np.array) -> np.array:
+    return np.append(array, [arrayToAppend], axis=0)
 
 
 def _getAmount(dataset: np.array, amount: int):
-    print(len(dataset))
     return amount if amount > ALL_HITS else len(dataset)
--- appTest.py
+++ appTest.py
@@ -1,42 +1,150 @@
+from python_helper import log
+from globals import newGlobalsInstance
+globalsInstance = newGlobalsInstance(
+    __file__,
+    successStatus = True,
+    errorStatus = True,
+    infoStatus = True,
+    # debugStatus = True,
+    failureStatus = True
+)
+log.info(__name__, 'Importiong libraries')
+
+
+import time
 import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+from python_helper import Constant as c
+from python_helper import RandomHelper, StringHelper, ObjectHelper
+
 from utils import query as queryFrom
+from utils.query import Dataset, HashedDataset, DatasetKeys
 
 
-mnist = fetch_openml('mnist_784', cache=False)
+log.info(__name__, 'Libraries imported')
 
 
-def example():
-    # dataset = np.asarray(
-    #     [
-    #         [1,2,3,4],
-    #         [2,3,4,5],
-    #         [3,4,5,6]
-    #     ]
-    # )
-    # queries = np.asarray(
-    #     [
-    #         [1,2,3,4],
-    #         [2,3,4,5],
-    #         [3,4,5,6]
-    #     ]
-    # )
-    # print('\n\n'+3*'========================')
-    # print('FULL QUERIES')
-    # for query in queries:
-    #     print(f'\n\nquery:\n{query}\n\nfrom dataset:\n{dataset}\n\nhits:\n{queryFrom.dataset(query, dataset)}')
-    # print('\n\n'+3*'========================')
-    # print('NEAREST HIT QUERIES -> axis: -1')
-    # for query in queries:
-    #     print(f'\n\nquery:\n{query}\n\nfrom dataset:\n{dataset}\n\nhits:\n{queryFrom.dataset(query, dataset, amount=1)}')
-    # print('\n\n'+3*'========================')
-    # axis = 1
-    # print(f'NEAREST HIT QUERIES -> axis: {axis}')
-    # for query in queries:
-    #     print(f'\n\nquery:\n{query}\n\nfrom dataset:\n{dataset}\n\nhits:\n{queryFrom.dataset(query, dataset, amount=1, axis=axis)}')
+def plotSamples(data: np.array, target, samples: int = 1, shape: tuple = (28, 28)):
+    """Plot the first 5 images and their labels in a row."""
+    for index, (img, target) in enumerate(zip(data[:samples].reshape(tuple([samples, *list(shape)])), target[:samples])):
+        plt.subplot(151 + index)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(target)
+
+
+def loadDataset(datasetName: str, dataType: str, targetType: str, cache: bool = False) -> Dataset:
+    log.info(loadDataset, 'Loading dataset')
+    originalDataset = fetch_openml(datasetName, cache=cache)
+    dataset = Dataset(
+        originalDataset.data.astype(dataType).values,
+        originalDataset.target.astype(targetType)
+    )
+    log.debug(loadDataset, 'Dataset loaded')
+    return dataset
+
+
+def roundToInt(value):
+    return int(round(value))
+
+
+def resizeByAverage(dataset: Dataset, dataShape: tuple)-> Dataset:
+    log.info(resizeByAverage, f'Resizing dataset to {dataShape} by avarege')
+    return Dataset(
+        resizeDataByAverage(dataset.data, dataShape),
+        dataset.target
+    )
+
+
+def resizeDataByAverage(data: np.array, dataShape: tuple)-> Dataset:
+    log.debug(resizeDataByAverage, f'Resizing data to {dataShape} by avarege')
+    #TODO: actualy write this "flattenedData" thing
+    flattenedData = [
+        dataUnit for dataUnit in data
+    ]
+    stepSize = len(flattenedData[0]) / sum(dataShape)
+    resized = np.asarray(
+        [
+            [
+                np.mean(dataUnit[roundToInt(step * stepSize):]) if roundToInt((step+1) * stepSize) >= len(dataUnit) else np.mean(dataUnit[roundToInt(step * stepSize):roundToInt((step+1) * stepSize)]) for step in range(roundToInt(len(dataUnit) / stepSize))
+            ] for dataUnit in flattenedData
+        ]
+    )
+    log.debug(resizeDataByAverage, f'Original shape: {data.shape}')
+    log.debug(resizeDataByAverage, f'Step size: {stepSize}')
+    log.debug(resizeDataByAverage, f'Resized shape: {resized.shape}')
+    return resized
+
+
+def resizeSampleByAverage(query: np.array, displayShape: tuple) -> np.array:
+    return resizeDataByAverage(
+        np.asarray([query]),
+        (np.product(np.asarray(displayShape)),)
+    )[0]
+
+
+def reshape(dataset: Dataset, shape: tuple, weigth: float = 1.0, displayShape: tuple = None) -> Dataset:
+    log.info(reshape, f'Reshaping dataset to {shape}')
+    reshapeDataset = None
+    try:
+        reshapeDataset = Dataset(
+            dataset.data.reshape(*shape) * weigth,
+            dataset.target,
+            displayShape = displayShape
+        )
+    except Exception as exception:
+        log.error(reshape, f'No possible to reshape {queryFrom.arrayToString(dataset.data)}', exception, muteStackTrace=True)
+        raise exception
+    reshapeDataset.displaySample()
+    return reshapeDataset
+
+
+def reduceShape(dataset: Dataset, shape: tuple) -> Dataset:
+    return resizeByAverage(dataset, (np.product(np.asarray(shape)),))
+
+
+def getHashedDataset(resampledMnist: Dataset, displayShape: tuple) -> dict:
+    log.info(getHashedDataset, 'Building hashed dataset over central point')
+    hashedDataset = HashedDataset(resampledMnist, displayShape)
+    log.debug(getHashedDataset, f'hashedDataset keys length: {len(list(hashedDataset.get()[DatasetKeys.VALUES].keys()))}')
+    log.debug(getHashedDataset, f'hashedDataset sampledDigit: {queryFrom.arrayToString(queryFrom.buildDatasetMean(RandomHelper.sample(list(hashedDataset.get().values()))[DatasetKeys.VALUES]), shape=displayShape)}')
+    return hashedDataset
+
+
+def mnist():
+    mnistDataset = loadDataset('mnist_784', 'float32', 'int64')
+    flattenMnist = reshape(mnistDataset, (-1, 784), weigth=1/255, displayShape=(28, 28))
+    # reshapedMnist = reshape(mnistDataset, (-1, 28, 28), weigth=1/255, displayShape=(28, 28))
+    reducedDisplaySchape = (3, 3)
+    resampledMnist = reduceShape(flattenMnist, reducedDisplaySchape)
+    reshapedResampledMnist = reshape(resampledMnist, (-1, *reducedDisplaySchape))
+    hashedDataset = getHashedDataset(resampledMnist, reducedDisplaySchape)
+
+    startedAt = time.time()
+    log.info(mnist, f'Query started at {startedAt}')
+    sampledIndex = RandomHelper.integer(minimum=0, maximum=flattenMnist.data.shape[0]-1)
+    predictions = hashedDataset.query(resizeSampleByAverage(flattenMnist.data[sampledIndex], reducedDisplaySchape), 10)
+    results = []
+    for prediction in predictions:
+        results.append(
+            hashedDataset.get()[DatasetKeys.VALUES][
+                queryFrom.arrayToString(
+                    resizeSampleByAverage(prediction, hashedDataset.displayShape),
+                    roundTo=2,
+                    shape=hashedDataset.displayShape
+                )
+            ][DatasetKeys.TARGET]
+        )
+    log.success(mnist, f'Expected result: {flattenMnist.target[sampledIndex]}')
+    log.success(mnist, f'Predicted result: {sorted(results, key=results.count, reverse=True)[0]}')
+    finishedAt = time.time()
+    log.info(mnist, f'Query finished at {finishedAt}')
+    log.info(mnist, f'Query last {finishedAt - startedAt} seconds')
+
 
 if __name__ == '__main__':
-    example()
+    mnist()
